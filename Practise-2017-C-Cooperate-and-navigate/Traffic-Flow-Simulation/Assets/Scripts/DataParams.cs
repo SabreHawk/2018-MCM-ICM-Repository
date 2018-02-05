7aeac188472f77993d86aa42fbf42bfa694d127e
Zhiquan.Wang <Zhiquan.Wang>
2018-02-05 16:27:23 +0800

    tmp update

---
+++ Practise-2017-C-Cooperate-and-navigate/Traffic-Flow-Simulation/Assets/Scripts/DataParams.cs
@@ -0,0 +1,16 @@
+﻿public class RoadParams{
+    public static float meter2feet = 5280f / 1609.344f;
+    public static float mile2feet = 5280f;
+    public static float road_len_5 = (215.51f - 100.93f) * mile2feet;
+    public static float road_len_90 = (25.37f - 1.94f) * mile2feet;
+    public static float road_len_405 = (30.32f - 0f) * mile2feet;
+    public static float road_len_520 = (12.83f - 0f) * mile2feet;
+
+
+}
+
+public class CarParams{
+	public static float min_safe_time = 3f;
+	public static float max_safe_time = 5f;
+}
+
---
+++ Practise-2017-C-Cooperate-and-navigate/Traffic-Flow-Simulation/Assets/Scripts/DistanceDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceDetector : MonoBehaviour {
+
+	private Rigidbody2D car_rigidbody;
+	private NormalCarController car_controller;
+	// Use this for initialization
+	void Start () {
+		car_rigidbody = this.GetComponentInParent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D _other){
+		float car_dis = Vector2.Distance (_other.transform.position, _other.transform.position);
+		float safe_dis = car_rigidbody.velocity.magnitude * Random.Range (CarParams.min_safe_time, CarParams.max_safe_time);
+		
+	}
+}
