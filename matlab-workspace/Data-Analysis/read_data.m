f4ed1eea07b3962f1c29aac578e5e5a340a322c1
simin zhan <m18752016299_1@163.com>
2018-02-09 16:52:13 +0800

    add 5 data.txt 2018/02/09 - 16:52

--- matlab-workspace/Data-Analysis/read_data.m
+++ matlab-workspace/Data-Analysis/read_data.m
@@ -4,12 +4,12 @@ data_dir = '..\ProblemCData.xlsx';
 raw_m = raw_m(2:end,:);
 [raw_m_len,~] = size(raw_m);
 
-total_consumption = importdata('total_consumption.txt');
-total_consumption_len = length(total_consumption);
+data = importdata('transportation_consumption.txt');
+total_consumption_len = length(data);
 A = zeros(50,4,total_consumption_len);
 for i = 1:raw_m_len
     for j = 1:total_consumption_len
-        if strcmp(raw_m{i,1},total_consumption(j))
+        if strcmp(raw_m{i,1},data(j))
             if strcmp(raw_m{i,2},'AZ')
             	A(raw_m{i,3}-1959,1,j) = raw_m{i,4};
             elseif strcmp(raw_m{i,2},'CA')
